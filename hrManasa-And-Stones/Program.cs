b3e41aa9026aed5045c9c4436befdaf92c3dca75
reggaeguitar <reggaeguitar@users.noreply.github.com>
2015-04-15 16:49:29 -0500

    Initial commit

---
+++ hrManasa-And-Stones/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace hrManasa_And_Stones
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        private static int _stones;
+        private static int _a;
+        private static int _b;
+        private static HashSet<int> _answers;
+
+        static void Main()
+        {
+            int numCases = Int32.Parse(Console.ReadLine());
+            while (numCases-- > 0)
+            {
+                _stones = Int32.Parse(Console.ReadLine()) - 1;
+                _a = Int32.Parse(Console.ReadLine());
+                _b = Int32.Parse(Console.ReadLine());
+                _answers = new HashSet<int>();
+                Solve(0, 0);
+                foreach (var answer in _answers.OrderBy(x => x))
+                {
+                    Console.Write(answer + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void Solve(int curVal, int depth)
+        {
+            if (depth == _stones)
+            {
+                _answers.Add(curVal);
+            }
+            else
+            {
+                Solve(curVal + _a, depth + 1);
+                Solve(curVal + _b, depth + 1);
+            }
+        }
+    }
+}
---
+++ hrManasa-And-Stones2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace hrManasa_And_Stones
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        private static int _stones;
+        private static int _a;
+        private static int _b;
+        private static List<int> _answers;
+
+        static void Main()
+        {
+            int numCases = Int32.Parse(Console.ReadLine());
+            while (numCases-- > 0)
+            {
+                _stones = Int32.Parse(Console.ReadLine()) - 1;
+                _a = Int32.Parse(Console.ReadLine());
+                _b = Int32.Parse(Console.ReadLine());
+                _answers = new List<int>();
+                if (_a > _b)
+                {
+                    int tmp = _a;
+                    _a = _b;
+                    _b = tmp;
+                }
+                _answers.Add(_stones * _a);
+                int dif = _b - _a;
+                for (int i = 1; i <= _stones; i++)
+                {
+                    _answers.Add(_answers[0] + (dif * i));
+                }
+                foreach (var answer in _answers)
+                {
+                    Console.Write(answer + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
